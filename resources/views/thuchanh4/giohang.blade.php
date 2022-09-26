ce53151fc6764c10e0d929f9894431e97072004a
John24 <dvcvochidung@gmail.com>
2022-09-26 22:19:00 +0700

    John deleted all file

--- resources/views/thuchanh4/giohang.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('thuchanh4/layout_thuchanh4')
-
-@section('middle-content')
-    <x-add_item />
-@endsection
--- resources/views/thuchanh4/viewcart.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('thuchanh4/layout_thuchanh4')
-
-@section('middle-content')
-    <x-view_cart></x-view_cart>
-@endsection
